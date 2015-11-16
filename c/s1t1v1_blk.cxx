62dd974a0c0655533779372f94f65dcbfcebc166
Edgar Solomonik <solomonik@inf.ethz.ch>
2015-11-16 19:17:18 +0100

    its wrong

--- c/s1t1v1_blk.cxx
+++ c/s1t1v1_blk.cxx
@@ -79,6 +79,10 @@ void s1t1v1_sh_as_fs_blk(Tensor<> & A, Tensor<> & B, Tensor<> & C){
   int symC[5] = {NS, NS, C.sym[2], C.sym[2], C.sym[3]};
   int lenC[5] = {C.lens[0], C.lens[1], n, n, n};
   Tensor<> ZC(5, lenC, symC);
+  
+  int64_t sign_A;
+  if (A.sym[2] == SH) sign_A = 1;
+  else                sign_A = -1;
 
   int sym_SH4[] = {NS, NS, SH, NS};
   int sym_SH5[] = {NS, NS, SH, SH, NS};
@@ -90,11 +94,12 @@ void s1t1v1_sh_as_fs_blk(Tensor<> & A, Tensor<> & B, Tensor<> & C){
   ZC = Tensor<>(ZC, sym_SH5);
   ZC["ijabc"] += ZA["ikabc"]*ZB["kjabc"];
   ZC = Tensor<>(ZC, symC);
-  C["ijab"] -= ZC["ijabc"];
 
+
+  C["ijab"] -= ZC["ijabc"];
   C["ijab"] -= A["ikac"]*B["kjac"];
-  C["ijab"] += A["ikac"]*B["kjab"];
-  C["ijab"] -= A["ikab"]*B["kjac"];
+  C["ijab"] += sign_A*A["ikac"]*B["kjab"];
+  C["ijab"] -= sign_A*A["ikab"]*B["kjac"];
 
   Tensor<> SH_A(A, sym_SH4);
   Tensor<> SH_B(B, sym_SH4);
@@ -121,15 +126,15 @@ bool test_s1t1v1_blk(int64_t n, int64_t b, int sA, int sB, int sC, void (*func)(
 
   srand48(rank*347+23);
 
-  A.fill_random(1.0, 1.0);
-  B.fill_random(1.0, 1.0);
+  A.fill_random(-1.0, 1.0);
+  B.fill_random(-1.0, 1.0);
 
   C["ijab"] += A["ikac"]*B["kjcb"];
 
   func(A, B, C2);
 
-  C.print();
-  C2.print();
+//  C.print();
+//  C2.print();
 
   C2["ijab"] -= C["ijab"];
 
@@ -196,7 +201,6 @@ void test_s1t1v1_blk(int64_t n, int64_t b){
     else      printf("  symmetry preserving algorithm test FAILED.\n");
   }
 
-
   if (rank == 0)
     printf("Testing AS=SH*AS with n=%ld, b=%ld...\n", n, b);
 
@@ -215,17 +219,94 @@ void test_s1t1v1_blk(int64_t n, int64_t b){
     else      printf("  symmetry preserving algorithm test FAILED.\n");
   }
 
+}
 
+void test_sq4d(int64_t n, int64_t b){
+  int rank;
+  bool pass;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0) printf("Testing fast matrix square algorithm\n");
 
+  int sym_NS[] = {NS, NS, NS, NS};
+  int sym_AS[] = {NS, NS, AS, NS};
+  int sym_SH[] = {NS, NS, SH, NS};
+  int lens[] = {b, b, n, n};
+  Tensor<> A(4, lens, sym_NS);
+  Tensor<> A_cpy(4, lens, sym_NS);
+  Tensor<> A_sq(4, lens, sym_NS);
 
+  A.fill_random(-1.0, 1.0);
+
+  A_cpy["iiab"] += A["iiab"];
+  A["ijab"] = 0.0;
+  A["ijab"] += A_cpy["ijab"];
+  A_cpy["ijab"] = 0.0;
+  A_cpy["ijab"] += A["ijab"];
+
+  A_sq["ijab"] += A["ikac"]*A["kjcb"];
+  
+  Tensor<> A_AS(4, lens, sym_AS);
+  Tensor<> A_SH(4, lens, sym_SH);
+  Tensor<> A_diag(4, lens, sym_NS);
+
+  A_diag["ijaa"] += A_cpy["ijaa"];
+//  A_diag.print();
+  A_cpy["ijab"] -= A_diag["ijab"];
+
+  A_AS["ijab"] += .5*A_cpy["ijab"];
+  A_SH["ijab"] += .5*A_cpy["ijab"];
+
+/*  A_cpy["ijab"] -= A_AS["ijab"];
+  A_cpy["ijab"] -= A_SH["ijab"];
+
+  assert(A_cpy.norm2() <= 1.e-8);*/
+
+  Tensor<> A_sq_fast(4, lens, sym_NS);
+
+  A_sq_fast["ijab"] += A["ikac"]*A_diag["kjcb"];
+  A_sq_fast["ijab"] += A_diag["ikac"]*A["kjcb"];
+  A_sq_fast["ijab"] -= A_diag["ikac"]*A_diag["kjcb"];
+  A_sq_fast["ijaa"] += A_cpy["ikac"]*A_cpy["kjca"];
+  
+  A_sq_fast["ijab"] *= 2.0;
+
+  //A_sq_fast.print();
+
+  Tensor<> A_sq_fast_SH(4, lens, sym_SH);
+  Tensor<> A_sq_fast_AS(4, lens, sym_AS)
+;
+
+  s1t1v1_sh_sh_fs_blk(A_SH, A_SH, A_sq_fast_SH);
+  s1t1v1_as_as_fs_blk(A_AS, A_AS, A_sq_fast_SH);
+  s1t1v1_sh_as_fs_blk(A_SH, A_AS, A_sq_fast_AS);
+  s1t1v1_sh_as_fs_blk(A_AS, A_SH, A_sq_fast_AS);
+
+  A_sq_fast["ijab"] += A_sq_fast_SH["ijab"];
+  A_sq_fast["ijab"] += A_sq_fast_AS["ijab"];
+
+  A_sq.print();
+  A_sq_fast.print();
+
+  A_sq_fast["ijab"] -= 2.*A_sq["ijab"];
+
+  A_sq_fast.print();
+
+  double err = A_sq_fast.norm2();
+
+  if (rank == 0){
+    if (err < 1.E-9) printf("  test successful!\n");
+    else             printf("  test FAILED, error of norm %lf!\n",err);
+  }
 }
 
 int main(int argc, char ** argv){
-  int64_t n = 4, b = 1;
+  int64_t n = 13, b = 4;
 
   MPI_Init(&argc, &argv);
 
   test_s1t1v1_blk(n,b);
+
+  test_sq4d(n,b);
 
   MPI_Finalize();
   return 0;
