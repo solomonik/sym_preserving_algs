86a9a8d080c5a482e1ef839cd163c960042c7f61
Edgar Solomonik <solomonik@inf.ethz.ch>
2015-11-16 17:21:21 +0100

    have symmetry preserving alg working for 3/4 versions

--- c/s1t1v1_blk.cxx
+++ c/s1t1v1_blk.cxx
@@ -2,39 +2,134 @@
 using namespace CTF;
 
 void s1t1v1_st_blk(Tensor<> & A, Tensor<> & B, Tensor<> & C){
+  int n = A.lens[2];
   int symA[5] = {NS, NS, NS, A.sym[2], A.sym[3]};
-  int lenA[5] = {A.lens[0], A.lens[1], C.lens[2], A.lens[2], A.lens[3]};
+  int lenA[5] = {A.lens[0], A.lens[1], n, n, n};
   Tensor<> ZA(5, lenA, symA);
   int symB[5] = {NS, NS, NS, B.sym[2], B.sym[3]};
-  int lenB[5] = {B.lens[0], B.lens[1], C.lens[3], B.lens[2], B.lens[3]};
-  ZA["ikcab"] += A["ikab"];
+  int lenB[5] = {B.lens[0], B.lens[1], n, n, n};
   Tensor<> ZB(5, lenB, symB);
+  int symC[5] = {NS, NS, NS, C.sym[2], C.sym[3]};
+  int lenC[5] = {C.lens[0], C.lens[1], n, n, n};
+  Tensor<> ZC(5, lenC, symC);
+  ZA["ikcab"] += A["ikab"];
   ZB["kjcab"] += B["kjab"];
-//  Tensor<> ZC(5, len, sym);
-  C["ijab"] += ZA["ikabc"]*ZB["kjbca"];
-//  C["ijab"] += ZC["ijcab"];
+  C["ijab"] += ZA["ikbac"]*ZB["kjacb"];
+  //ZC["ijcab"] += ZA["ikabc"]*ZB["kjbca"];
+  //C["ijab"] += ZC["ijcab"];
 }
 
-bool test_s1t1v1_st_blk(int64_t n, int64_t b){
-  int lens[] = {b, b, n, n};
-  printf("Testing block direct evaluation algorithm for SY=SY*SY with n=%ld, b=%ld... ", n, b);
 
-  int sym_SY[] = {NS, NS, SY, NS};
-  Tensor<> A(4, lens, sym_SY);
-  Tensor<> B(4, lens, sym_SY);
-  Tensor<> C(4, lens, sym_SY);
-  Tensor<> C2(4, lens, sym_SY);
+void s1t1v1_sh_sh_fs_blk(Tensor<> & A, Tensor<> & B, Tensor<> & C){
+  int n = A.lens[2];
+  int symA[5] = {NS, NS, A.sym[2], A.sym[2], A.sym[3]};
+  int lenA[5] = {A.lens[0], A.lens[1], n, n, n};
+  Tensor<> ZA(5, lenA, symA);
+  int symB[5] = {NS, NS, B.sym[2], B.sym[2], B.sym[3]};
+  int lenB[5] = {B.lens[0], B.lens[1], n, n, n};
+  Tensor<> ZB(5, lenB, symB);
+  int symC[5] = {NS, NS, C.sym[2], C.sym[2], C.sym[3]};
+  int lenC[5] = {C.lens[0], C.lens[1], n, n, n};
+  Tensor<> ZC(5, lenC, symC);
+  ZA["ikcab"] += A["ikab"];
+  ZB["kjcab"] += B["kjab"];
+  ZC["ijabc"] += ZA["ikabc"]*ZB["kjabc"];
+  C["ijab"] += ZC["ijabc"];
+  C["ijab"] -= A["ikac"]*B["kjac"];
+  C["ijab"] -= A["ikac"]*B["kjab"];
+  C["ijab"] -= A["ikab"]*B["kjac"];
+  C["ijab"] -= ((int64_t)n-8)*A["ikab"]*B["kjab"];
+}
 
+
+void s1t1v1_as_as_fs_blk(Tensor<> & A, Tensor<> & B, Tensor<> & C){
+  int n = A.lens[2];
+  int symA[5] = {NS, NS, A.sym[2], A.sym[2], A.sym[3]};
+  int lenA[5] = {A.lens[0], A.lens[1], n, n, n};
+  Tensor<> ZA(5, lenA, symA);
+  int symB[5] = {NS, NS, B.sym[2], B.sym[2], B.sym[3]};
+  int lenB[5] = {B.lens[0], B.lens[1], n, n, n};
+  Tensor<> ZB(5, lenB, symB);
+  int symC[5] = {NS, NS, C.sym[2], C.sym[2], C.sym[3]};
+  int lenC[5] = {C.lens[0], C.lens[1], n, n, n};
+  Tensor<> ZC(5, lenC, symC);
+  ZA["ikabc"] += A["ikab"];
+  ZB["kjabc"] += B["kjab"];
+  ZA = Tensor<>(ZA, symC);
+  ZB = Tensor<>(ZB, symC);
+  ZC["ijabc"] += ZA["ikabc"]*ZB["kjabc"];
+  C["ijab"] += ZC["ijabc"];
+  int sym_SH[] = {NS, NS, SH, NS};
+  Tensor<> SH_A(A, sym_SH);
+  Tensor<> SH_B(B, sym_SH);
+  C["ijab"] -= SH_A["ikac"]*SH_B["kjac"];
+  C["ijab"] += A["ikac"]*B["kjab"];
+  C["ijab"] += A["ikab"]*B["kjac"];
+  C["ijab"] -= ((double)n)*SH_A["ikab"]*SH_B["kjab"];
+}
+
+void s1t1v1_sh_as_fs_blk(Tensor<> & A, Tensor<> & B, Tensor<> & C){
+  int n = A.lens[2];
+  int symA[5] = {NS, NS, A.sym[2], A.sym[2], A.sym[3]};
+  int lenA[5] = {A.lens[0], A.lens[1], n, n, n};
+  Tensor<> ZA(5, lenA, symA);
+  int symB[5] = {NS, NS, B.sym[2], B.sym[2], B.sym[3]};
+  int lenB[5] = {B.lens[0], B.lens[1], n, n, n};
+  Tensor<> ZB(5, lenB, symB);
+  int symC[5] = {NS, NS, C.sym[2], C.sym[2], C.sym[3]};
+  int lenC[5] = {C.lens[0], C.lens[1], n, n, n};
+  Tensor<> ZC(5, lenC, symC);
+
+  int sym_SH4[] = {NS, NS, SH, NS};
+  int sym_SH5[] = {NS, NS, SH, SH, NS};
+
+  ZA["ikabc"] += A["ikab"];
+  ZB["kjabc"] += B["kjab"];
+  ZA = Tensor<>(ZA, sym_SH5);
+  ZB = Tensor<>(ZB, sym_SH5);
+  ZC = Tensor<>(ZC, sym_SH5);
+  ZC["ijabc"] += ZA["ikabc"]*ZB["kjabc"];
+  ZC = Tensor<>(ZC, symC);
+  C["ijab"] -= ZC["ijabc"];
+
+  C["ijab"] -= A["ikac"]*B["kjac"];
+  C["ijab"] += A["ikac"]*B["kjab"];
+  C["ijab"] -= A["ikab"]*B["kjac"];
+
+  Tensor<> SH_A(A, sym_SH4);
+  Tensor<> SH_B(B, sym_SH4);
+  Tensor<> SH_C(C, sym_SH4);
+  SH_C["ijab"] += ((double)n)*SH_A["ikab"]*SH_B["kjab"];
+  A = Tensor<>(SH_A, A.sym);
+  B = Tensor<>(SH_B, B.sym);
+  C = Tensor<>(SH_C, C.sym);
+}
+
+
+bool test_s1t1v1_blk(int64_t n, int64_t b, int sA, int sB, int sC, void (*func)(Tensor<>&,Tensor<>&,Tensor<>&)){
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  int lens[] = {b, b, n, n};
+
+  int sym_A[] = {NS, NS, sA, NS};
+  int sym_B[] = {NS, NS, sB, NS};
+  int sym_C[] = {NS, NS, sC, NS};
+  Tensor<> A(4, lens, sym_A);
+  Tensor<> B(4, lens, sym_B);
+  Tensor<> C(4, lens, sym_C);
+  Tensor<> C2(4, lens, sym_C);
+
   srand48(rank*347+23);
 
-  A.fill_random(-1.0, 1.0);
-  B.fill_random(-1.0, 1.0);
+  A.fill_random(1.0, 1.0);
+  B.fill_random(1.0, 1.0);
 
   C["ijab"] += A["ikac"]*B["kjcb"];
 
-  s1t1v1_st_blk(A, B, C2);
+  func(A, B, C2);
+
+  C.print();
+  C2.print();
 
   C2["ijab"] -= C["ijab"];
 
@@ -42,21 +137,95 @@ bool test_s1t1v1_st_blk(int64_t n, int64_t b){
   double norm_dif = C2.norm2();
   bool pass = norm_ans>=0.0 & norm_dif<=1.E-8*n*b;
 
-  if (rank == 0){
-    if (pass) printf("direct evaluation algorithm test passed.\n");
-    else      printf("direct evaluation algorithm test FAILED, norm of answer is = %lf, norm of delta is = %lf\n.", norm_ans, norm_dif);
-  }
+  if (!pass && rank == 0) printf("norm of answer is = %lf, norm of delta is = %lf\n.", norm_ans, norm_dif);
   return pass;
+}
+
+void test_s1t1v1_blk(int64_t n, int64_t b){
+  int rank;
+  bool pass;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if (rank == 0)
+    printf("Testing SH=SH*SH with n=%ld, b=%ld...\n", n, b);
+
+  pass = test_s1t1v1_blk(n, b, SH, SH, SH, &s1t1v1_st_blk);
+  
+  if (rank == 0){
+    if (pass) printf("  direct evaluation algorithm test passed.\n");
+    else      printf("  direct evaluation algorithm test FAILED.\n");
+  }
+
+  pass = test_s1t1v1_blk(n, b, SH, SH, SH, &s1t1v1_sh_sh_fs_blk);
+
+  if (rank == 0){
+    if (pass) printf("  symmetry preserving algorithm test passed.\n");
+    else      printf("  symmetry preserving algorithm test FAILED.\n");
+  }
+
+  if (rank == 0)
+    printf("Testing SH=AS*AS with n=%ld, b=%ld...\n", n, b);
+  
+  pass = test_s1t1v1_blk(n, b, AS, AS, SH, &s1t1v1_st_blk);
+ 
+  if (rank == 0){
+    if (pass) printf("  direct evaluation algorithm test passed.\n");
+    else      printf("  direct evaluation algorithm test FAILED.\n");
+  }
+
+  pass = test_s1t1v1_blk(n, b, AS, AS, SH, &s1t1v1_as_as_fs_blk);
+
+  if (rank == 0){
+    if (pass) printf("  symmetry preserving algorithm test passed.\n");
+    else      printf("  symmetry preserving algorithm test FAILED.\n");
+  }
+
+  if (rank == 0)
+    printf("Testing AS=AS*SH with n=%ld, b=%ld...\n", n, b);
+
+  pass = test_s1t1v1_blk(n, b, AS, SH, AS, &s1t1v1_st_blk);
+  
+  if (rank == 0){
+    if (pass) printf("  direct evaluation algorithm test passed.\n");
+    else      printf("  direct evaluation algorithm test FAILED.\n");
+  }
+
+  pass = test_s1t1v1_blk(n, b, AS, SH, AS, &s1t1v1_sh_as_fs_blk);
+
+  if (rank == 0){
+    if (pass) printf("  symmetry preserving algorithm test passed.\n");
+    else      printf("  symmetry preserving algorithm test FAILED.\n");
+  }
+
+
+  if (rank == 0)
+    printf("Testing AS=SH*AS with n=%ld, b=%ld...\n", n, b);
+
+  pass = test_s1t1v1_blk(n, b, SH, AS, AS, &s1t1v1_st_blk);
+  
+  if (rank == 0){
+    if (pass) printf("  direct evaluation algorithm test passed.\n");
+    else      printf("  direct evaluation algorithm test FAILED.\n");
+  }
+
+
+  pass = test_s1t1v1_blk(n, b, SH, AS, AS, &s1t1v1_sh_as_fs_blk);
+
+  if (rank == 0){
+    if (pass) printf("  symmetry preserving algorithm test passed.\n");
+    else      printf("  symmetry preserving algorithm test FAILED.\n");
+  }
+
+
+
 
 }
 
 int main(int argc, char ** argv){
-  int64_t n = 8, b = 4;
+  int64_t n = 4, b = 1;
 
   MPI_Init(&argc, &argv);
 
-  bool pass = test_s1t1v1_st_blk(n,b);
-  assert(pass);
+  test_s1t1v1_blk(n,b);
 
   MPI_Finalize();
   return 0;
