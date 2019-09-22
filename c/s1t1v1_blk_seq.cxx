1c1ba65f602e6bb4b7694a7c79daa3af1b574870
Edgar Solomonik <solomon2@illinois.edu>
2019-09-22 17:59:13 -0500

    get c code running

--- c/s1t1v1_blk_seq.cxx
+++ c/s1t1v1_blk_seq.cxx
@@ -1,6 +1,13 @@
 #include <limits>
 #include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <cstring>
+#include <algorithm>
 #include <sys/time.h>
+
+#define FTN_UNDERSCORE 1
+
 
 #if FTN_UNDERSCORE
 #define DGEMM dgemm_
@@ -12,6 +19,25 @@
 #define DGEMM_BATCH dgemm_batch
 #define DAXPY daxpy
 #endif
+
+void print_matrix(double const * M, int n, int m){
+  int i,j;
+  for (i = 0; i < n; i++){
+    for (j = 0; j < m; j++){
+      printf("%+2.4lf ", M[i+j*n]);
+    }
+    printf("\n");
+  }
+}
+void print_matrixd(double ** M, int n, int m){
+  int i,j;
+  for (i = 0; i < n; i++){
+    for (j = 0; j < m; j++){
+      printf("%+2.4lf ", *(M[i+j*n]));
+    }
+    printf("\n");
+  }
+}
 
 static double __timer(){
   static bool initialized = false;
@@ -72,9 +98,7 @@ void DAXPY(const int *    n,
 
 void gemm(int m, int n, int k, double alpha, double const * A, double const * B, double beta, double * C){
   char cN = 'N';
-  double alpha = 1.0;
-  double beta = 0.0;
-  DGEMM(&cN, &cN, &m, &n, &k, &alpha, A, &m, B, &k, &beta, &C, &m);
+  DGEMM(&cN, &cN, &m, &n, &k, &alpha, A, &m, B, &k, &beta, C, &m);
 }
 
 void axpy(int n, double alpha, double const * X, double * Y){
@@ -86,60 +110,74 @@ double * naive_gemm(double const * A, double const * B, int64_t n, int64_t b){
   double * C = new double[n*b*n*b];
   int m = n*b;
   assert(n*b == (int64_t)m);
-  //DGEMM(&cN, &cN, &m, &m, &m, &alpha, A, &m, B, &m, C, &m);
-  gemm(m,m,m,alpha,A,B,C);
+  double t_st = __timer();
+  gemm(m,m,m,1.,A,B,0.,C);
+  double t_end = __timer();
+  printf("gemm took %lf sec\n",t_end-t_st);
   //int mm = m*m;
   //assert(mm == ((int64_t)m)*((int64_t)m));
-  double * C2 = new double[n*b*n*b];
-  memcpy(C2, C, n*b*n*b*sizeof(double));
-  int one = 1;
-  for (int64_t i=0; i<m; i++){
-    DAXPY(&m, &alpha, C2+i, &m, C+i*m, &one);
+  //double * C2 = new double[n*b*n*b];
+  //memcpy(C2, C, n*b*n*b*sizeof(double));
+  //int one = 1;
+  //for (int64_t i=0; i<m; i++){
+  //  double alpha = 1.;
+  //  DAXPY(&m, &alpha, C2+i, &m, C+i*m, &one);
+  //}
+  for (int i=0; i<n; i++){
+    for (int j=0; j<=i; j++){
+      for (int k=0; k<b; k++){
+        for (int l=0; l<b; l++){
+          C[(j*b+k)*n*b+(i*b+l)] += C[(i*b+k)*n*b+(j*b+l)];
+          C[(i*b+k)*n*b+(j*b+l)]  = C[(j*b+k)*n*b+(i*b+l)];
+        }
+      }
+    }
   }
-  delete [] C2;
+
+  //delete [] C2;
   return C;
 }
 
 double * fast_gemm(double const * pA, double const * pB, int64_t n, int64_t b){
-  double * A = new double*[n*n];
-  double * B = new double*[n*n];
-  double * C = new double*[n*n];
+  double ** A = new double*[n*n];
+  double ** B = new double*[n*n];
+  double ** C = new double*[n*n];
   for (int64_t i=0; i<n; i++){
     for (int64_t j=0; j<=i; j++){
-      A[i,j] = new double[b*b];
-      A[j,i] = A[i,j];
-      B[i,j] = new double[b*b];
-      B[j,i] = B[i,j];
+      A[i*n+j] = new double[b*b];
+      A[j*n+i] = A[i*n+j];
+      B[i*n+j] = new double[b*b];
+      B[j*n+i] = B[i*n+j];
       for (int64_t ib=0; ib<b; ib++){
         for (int64_t jb=0; jb<b; jb++){
-          A[i,j][jb+ib*b] = pA[(i*b+ib)*(n*b) + (j*b+jb)];
-          B[i,j][jb+ib*b] = pB[(i*b+ib)*(n*b) + (j*b+jb)];
+          A[i*n+j][jb+ib*b] = pA[(i*b+ib)*(n*b) + (j*b+jb)];
+          B[i*n+j][jb+ib*b] = pB[(i*b+ib)*(n*b) + (j*b+jb)];
         }
       }
-      C[i,j] = new double[b*b];
-      C[j,i] = C[i,j];
-      std::fill(C[i,j], C[i,j]+b*b, 0.);
+      C[i*n+j] = new double[b*b];
+      C[j*n+i] = C[i*n+j];
+      std::fill(C[i*n+j], C[i*n+j]+b*b, 0.);
     }
   }
-  double * sA = new double*[n];
-  double * sB = new double*[n];
-  double * sC = new double*[n];
+  double ** sA = new double*[n];
+  double ** sB = new double*[n];
+  double ** sC = new double*[n];
   for (int64_t i=0; i<n; i++){
     sA[i] = new double[b*b];
     std::fill(sA[i], sA[i]+b*b, 0.);
     for (int64_t j=0; j<n; j++){
       if (i==j)
-        axpy(b*b,  1., A[i,j], sA[i]) 
+        axpy(b*b,  1., A[i*n+j], sA[i]);
       else 
-        axpy(b*b, -1., A[i,j], sA[i]) 
+        axpy(b*b, -1., A[i*n+j], sA[i]);
     }
     sB[i] = new double[b*b];
     std::fill(sB[i], sB[i]+b*b, 0.);
     for (int64_t j=0; j<n; j++){
       if (i==j)
-        axpy(b*b,  1., B[i,j], sB[i]) 
+        axpy(b*b,  1., B[i*n+j], sB[i]); 
       else 
-        axpy(b*b, -1., B[i,j], sB[i]) 
+        axpy(b*b, -1., B[i*n+j], sB[i]); 
     }
     sC[i] = new double[b*b];
     std::fill(sC[i], sC[i]+b*b, 0.);
@@ -153,78 +191,74 @@ double * fast_gemm(double const * pA, double const * pB, int64_t n, int64_t b){
         std::fill(tmp_A, tmp_A+b*b, 0.);
         std::fill(tmp_B, tmp_B+b*b, 0.);
         std::fill(tmp_C, tmp_C+b*b, 0.);
-        axpy(b*b, 1., A[i,j], tmp_A);
-        axpy(b*b, 1., A[i,k], tmp_A);
-        axpy(b*b, 1., A[j,k], tmp_A);
-        axpy(b*b, 1., B[i,j], tmp_B);
-        axpy(b*b, 1., B[i,k], tmp_B);
-        axpy(b*b, 1., B[j,k], tmp_B);
+        axpy(b*b, 1., A[i*n+j], tmp_A);
+        axpy(b*b, 1., A[i*n+k], tmp_A);
+        axpy(b*b, 1., A[j*n+k], tmp_A);
+        axpy(b*b, 1., B[i*n+j], tmp_B);
+        axpy(b*b, 1., B[i*n+k], tmp_B);
+        axpy(b*b, 1., B[j*n+k], tmp_B);
         gemm(b,b,b,1.0,tmp_A,tmp_B,1.0,tmp_C);
-        axpy(b*b, 1., tmp_C, C[i,j]);
-        axpy(b*b, 1., tmp_C, C[i,k]);
-        axpy(b*b, 1., tmp_C, C[j,k]);
+        axpy(b*b, 1., tmp_C, C[i*n+j]);
+        axpy(b*b, 1., tmp_C, C[i*n+k]);
+        axpy(b*b, 1., tmp_C, C[j*n+k]);
       }
     }
   }
   for (int64_t i=0; i<n; i++){
     for (int64_t j=0; j<i; j++){
-      gemm(b,b,b,1.0,A[i,j],sB[i],1.0,C[i,j]);
-      gemm(b,b,b,1.0,sA[i],B[i,j],1.0,C[i,j]);
+      gemm(b,b,b,1.0,A[i*n+j],sB[i],1.0,C[i*n+j]);
+      gemm(b,b,b,1.0,A[i*n+j],sB[j],1.0,C[i*n+j]);
+      gemm(b,b,b,1.0,sA[i],B[i*n+j],1.0,C[i*n+j]);
+      gemm(b,b,b,1.0,sA[j],B[i*n+j],1.0,C[i*n+j]);
       std::fill(tmp_C, tmp_C+b*b, 0.);
-      gemm(b,b,b,1.0,A[i,j],B[i,j],1.0,tmp_C);
-      axpy(b*b,  -(n-8.), tmp_C, C[i,j]) 
-      axpy(b*b,  1., tmp_C, sC[i]) 
-      axpy(b*b,  1., tmp_C, sC[j])
+      gemm(b,b,b,1.0,A[i*n+j],B[i*n+j],1.0,tmp_C);
+      axpy(b*b,  -(n-8.), tmp_C, C[i*n+j]); 
+      axpy(b*b,  1., tmp_C, sC[i]);
+      axpy(b*b,  1., tmp_C, sC[j]);
     }
   } 
   for (int64_t i=0; i<n; i++){
     for (int64_t j=0; j<i; j++){
-      axpy(b*b, -1., sC[i], C[i,j]) 
-      axpy(b*b, -1., sC[j], C[i,j]) 
+      axpy(b*b, -1., sC[i], C[i*n+j]);
+      axpy(b*b, -1., sC[j], C[i*n+j]); 
     }
-    axpy(b*b, 2., sC[i], C[i,i]) 
-    gemm(b,b,b,2.0,A[i,i],B[i,i],1.0,C[i,i]);
+    axpy(b*b, 2., sC[i], C[i*n+i]);
+    gemm(b,b,b,2.0,A[i*n+i],B[i*n+i],1.0,C[i*n+i]);
   }
+
   double * pC = new double[n*b*n*b];
   for (int64_t i=0; i<n; i++){
     for (int64_t j=0; j<=i; j++){
       for (int64_t ib=0; ib<b; ib++){
         for (int64_t jb=0; jb<b; jb++){
-          pC[(i*b+ib)*(n*b) + (j*b+jb)] = C[i,j][jb+ib*b];
+          pC[(i*b+ib)*(n*b) + (j*b+jb)] = C[i*n+j][jb+ib*b];
           if (j!=i)
-            pC[(j*b+jb)*(n*b) + (i*b+ib)] = C[i,j][jb+ib*b];
+            pC[(j*b+ib)*(n*b) + (i*b+jb)] = C[i*n+j][jb+ib*b];
         }
       }
-      delete A[i,j]
-      delete B[i,j]
-      delete C[i,j]
     }
-    delete sA[i];
-    delete sB[i];
-    delete sC[i];
   }
-  delete tmp_A;
-  delete tmp_B;
-  delete tmp_C;
-  delete sA;
-  delete sB;
-  delete sC;
-  delete A;
-  delete B;
-  delete C;
-
   for (int64_t i=0; i<n; i++){
-    sC[i] = new double[b*b];
-    std::fill(sC[i], sA[i]+b*b, 0.);
-    for (int64_t j=0; j<n; j++){
-      if (i==j)
-        axpy(b*b,  1., A[i,j], sA[i]) 
-      else 
-        axpy(b*b, -1., A[i,j], sA[i]) 
+    for (int64_t j=0; j<=i; j++){
+      delete [] A[i*n+j];
+      delete [] B[i*n+j];
+      delete [] C[i*n+j];
     }
+    delete [] sA[i];
+    delete [] sB[i];
+    delete [] sC[i];
   }
+  delete [] tmp_A;
+  delete [] tmp_B;
+  delete [] tmp_C;
+  delete [] sA;
+  delete [] sB;
+  delete [] sC;
+  delete [] A;
+  delete [] B;
+  delete [] C;
 
-  return C;
+  return pC;
 }
 
 
@@ -246,9 +280,9 @@ void bench_s1t1v1_blk(int64_t n, int64_t b, int niter){
     for (int j=0; j<=i; j++){
       for (int k=0; k<b; k++){
         for (int l=0; l<b; l++){
-          A[(i*b+k)*n*b+(j*b+l)] = drand48();
+          A[(i*b+k)*n*b+(j*b+l)] = drand48()-.5;
           A[(j*b+k)*n*b+(i*b+l)] = A[(i*b+k)*n*b+(j*b+l)];
-          B[(i*b+k)*n*b+(j*b+l)] = drand48();
+          B[(i*b+k)*n*b+(j*b+l)] = drand48()-.5;
           B[(j*b+k)*n*b+(i*b+l)] = B[(i*b+k)*n*b+(j*b+l)];
         }
       }
@@ -256,17 +290,17 @@ void bench_s1t1v1_blk(int64_t n, int64_t b, int niter){
   }
   printf("Naive times:\n");
   for (int i=0; i<niter; i++){
-    double t_st = __time();
+    double t_st = __timer();
     double * C = naive_gemm(A,B,n,b);
-    double t_end = __time();
+    double t_end = __timer();
     printf("%lf\n",t_end-t_st);
     delete [] C;
   }
   printf("Fast times:\n");
   for (int i=0; i<niter; i++){
-    double t_st = __time();
+    double t_st = __timer();
     double * C = fast_gemm(A,B,n,b);
-    double t_end = __time();
+    double t_end = __timer();
     printf("%lf\n",t_end-t_st);
     delete [] C;
   }
@@ -280,9 +314,9 @@ void test_s1t1v1_blk(int64_t n, int64_t b){
     for (int j=0; j<=i; j++){
       for (int k=0; k<b; k++){
         for (int l=0; l<b; l++){
-          A[(i*b+k)*n*b+(j*b+l)] = drand48();
+          A[(i*b+k)*n*b+(j*b+l)] = drand48()-.5;
           A[(j*b+k)*n*b+(i*b+l)] = A[(i*b+k)*n*b+(j*b+l)];
-          B[(i*b+k)*n*b+(j*b+l)] = drand48();
+          B[(i*b+k)*n*b+(j*b+l)] = drand48()-.5;
           B[(j*b+k)*n*b+(i*b+l)] = B[(i*b+k)*n*b+(j*b+l)];
         }
       }
@@ -290,6 +324,10 @@ void test_s1t1v1_blk(int64_t n, int64_t b){
   }
   double * C_ref = naive_gemm(A,B,n,b);
   double * C_fst = fast_gemm(A,B,n,b);
+  //printf("C_ref:\n");
+  //print_matrix(C_ref,n*b,n*b);
+  //printf("C_fst:\n");
+  //print_matrix(C_fst,n*b,n*b);
   double err = 0.;
   double nrm = 0.;
   for (int i=0; i<n; i++){
@@ -311,7 +349,7 @@ int main(int argc, char ** argv){
   int const in_num = argc;
   char ** input_str = argv;
 
-  int64_t n, b;
+  int64_t n, b, niter;
   if (getCmdOption(input_str, input_str+in_num, "-n")){
     n = atoi(getCmdOption(input_str, input_str+in_num, "-n"));
     if (n < 0) n = 4;
@@ -320,7 +358,14 @@ int main(int argc, char ** argv){
     b = atoi(getCmdOption(input_str, input_str+in_num, "-b"));
     if (b < 0) b = 3;
   } else b = 3;
+  if (getCmdOption(input_str, input_str+in_num, "-niter")){
+    niter = atoi(getCmdOption(input_str, input_str+in_num, "-niter"));
+    if (niter < 0) niter = 3;
+  } else niter = 3;
 
+  printf("Testing n=%ld b=%ld\n",n,b);
   test_s1t1v1_blk(n,b);
+  printf("Benchmarking n=%ld b=%ld niter=%d\n",n,b,niter);
+  bench_s1t1v1_blk(n,b,niter);
   return 0;
 }
